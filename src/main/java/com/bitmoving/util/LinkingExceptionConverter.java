95f499fe56f587d4a5b9c2aeffd1ef9b842d432c
Benno Kittelmann <benno@bitmoving.com>
2015-04-04 17:40:44 +0200

    Custom converter that adds links to the HTML pages showing source code of the classes on the stack trace - WIP

---
+++ src/main/java/com/bitmoving/util/LinkingExceptionConverter.java
@@ -0,0 +1,49 @@
+package com.bitmoving.util;
+
+
+import ch.qos.logback.classic.pattern.ClassicConverter;
+import ch.qos.logback.classic.pattern.ThrowableHandlingConverter;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.classic.spi.IThrowableProxy;
+import ch.qos.logback.classic.spi.StackTraceElementProxy;
+import ch.qos.logback.classic.spi.ThrowableProxy;
+
+public class LinkingExceptionConverter extends ThrowableHandlingConverter {
+
+    private String base = "https://github.com/bkittelmann/flowdock-appender/tree/master/src/main/java/";
+
+    private String rootPackage = "com.bitmoving";
+
+    @Override
+    public String convert(ILoggingEvent event) {
+        IThrowableProxy itp = event.getThrowableProxy();
+        if (itp instanceof ThrowableProxy) {
+            System.out.println("In linking exception converter");
+            ThrowableProxy tp = (ThrowableProxy)itp;
+
+            StringBuilder builder = new StringBuilder();
+
+            for (StackTraceElementProxy step: tp.getStackTraceElementProxyArray()) {
+                String className = step.getStackTraceElement().getClassName();
+                if (className.startsWith(rootPackage)) {
+                    String fileName = step.getStackTraceElement().getFileName();
+                    String methodName = step.getStackTraceElement().getMethodName();
+                    int lineNumber = step.getStackTraceElement().getLineNumber();
+
+                    String replacement = "." + fileName + "#L" + lineNumber; //"%s %s".format(step.getStackTraceElement().getFileName(), step.getStackTraceElement().getLineNumber());
+                    System.out.println("Replacaement: " + replacement);
+                    String result = className.replace(".", "/"). replaceAll("/\\w+$", replacement);
+                    System.out.println(result);
+                    builder.append(String.format("<br/>at %s.%s(<a href=\"%s\">%s</a>)", className, methodName, base + result, fileName + ":" + lineNumber));
+                } else {
+                    builder.append("<br/>" + step.getSTEAsString());
+                }
+
+            }
+
+            return builder.toString();
+        }
+
+        return "";
+    }
+}
