76a75798692fa19fa8e5a57d450e5872ef8dea43
Benno Kittelmann <benno@bitmoving.com>
2015-04-04 17:27:35 +0200

    More configurable and test-friendly appender

--- src/main/java/com/bitmoving/util/FlowdockAppender.java
+++ src/main/java/com/bitmoving/util/FlowdockAppender.java
@@ -4,20 +4,26 @@ package com.bitmoving.util;
 import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.AppenderBase;
+import ch.qos.logback.core.encoder.LayoutWrappingEncoder;
+import ch.qos.logback.core.util.StatusPrinter;
 import com.ning.http.client.AsyncHttpClient;
 import com.ning.http.client.Request;
 import com.ning.http.client.RequestBuilder;
+import com.ning.http.client.Response;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 
 public class FlowdockAppender extends AppenderBase<ILoggingEvent> {
-    // configurable through logback config
-    private PatternLayoutEncoder encoder;
-    private String flowToken;
-    private String author;
+    public static final String DEFAULT_ENDPOINT = "https://api.flowdock.com/messages";
 
     // not configurable from logback config
     final AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
+    // configurable through logback config
+    private LayoutWrappingEncoder encoder;
+    private String flowToken;
+    private String apiEndpoint = DEFAULT_ENDPOINT;
+    private String author;
 
     @Override
     public void start() {
@@ -54,11 +60,11 @@ public class FlowdockAppender extends AppenderBase<ILoggingEvent> {
         this.author = author;
     }
 
-    public PatternLayoutEncoder getEncoder() {
+    public LayoutWrappingEncoder getEncoder() {
         return encoder;
     }
 
-    public void setEncoder(PatternLayoutEncoder encoder) {
+    public void setEncoder(LayoutWrappingEncoder encoder) {
         this.encoder = encoder;
     }
 
@@ -70,25 +76,40 @@ public class FlowdockAppender extends AppenderBase<ILoggingEvent> {
         this.flowToken = flowToken;
     }
 
+    public String getApiEndpoint() {
+        return apiEndpoint;
+    }
+
+    public void setApiEndpoint(String apiEndpoint) {
+        this.apiEndpoint = apiEndpoint;
+    }
+
     private String buildJson(ILoggingEvent event) throws IOException {
         return new MessageBuilder(flowToken, author, encoder).build(event);
     }
 
     protected void sendRequest(ILoggingEvent event) {
-        final String url = "https://api.flowdock.com/messages";
-
         try {
             String json = buildJson(event);
+
             RequestBuilder requestBuilder = new RequestBuilder("POST");
             Request request = requestBuilder
-                    .setUrl(url)
-                    .setBody(json)
-                    .addHeader("Content-Type", "application/json")
-                    .build();
+                .setUrl(apiEndpoint)
+                .setBody(json)
+                .addHeader("Content-Type", "application/json")
+                .build();
 
-            asyncHttpClient.executeRequest(request);
+            Response response = asyncHttpClient.executeRequest(request).get();
+            int statusCode = response.getStatusCode();
+            if (statusCode > 299) {
+                addError(String.format("Could not store in flowdock: %s %s", statusCode, response.getStatusText()));
+            }
         } catch (IOException e) {
             addError("Could not send request to Flowdock", e);
+        } catch (InterruptedException e) {
+            addError("Exception while sending message", e);
+        } catch (ExecutionException e) {
+            addError("Exception while sending message", e);
         }
     }
 }
