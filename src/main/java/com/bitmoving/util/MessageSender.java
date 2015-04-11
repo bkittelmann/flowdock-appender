61abb76b2973607c18f2ae959b2c07a7739414ca
Benno Kittelmann <benno@bitmoving.com>
2015-04-11 20:56:07 +0200

    Use ExecutorService to execute requests asynchronously

--- src/main/java/com/bitmoving/util/MessageSender.java
+++ src/main/java/com/bitmoving/util/MessageSender.java
@@ -7,10 +7,14 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.Charset;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class MessageSender {
     private final URL endpoint;
     private final ContextAware appender;
+    private ExecutorService executor =  Executors.newFixedThreadPool(10);
 
     public MessageSender(URL endpoint, ContextAware appender) {
         this.endpoint = endpoint;
@@ -18,6 +22,39 @@ public class MessageSender {
     }
 
     public void sendRequest(String json) {
+        RequestRunnable runnable = new RequestRunnable(endpoint, json, appender);
+        executor.execute(runnable);
+    }
+
+    public void stop() {
+        executor.shutdown();
+        try {
+            executor.awaitTermination(10, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            appender.addError("Problem waiting for request execution to finish", e);
+        }
+    }
+
+    public void setExecutorService(ExecutorService executorService) {
+        executor = executorService;
+    }
+}
+
+class RequestRunnable implements Runnable {
+
+    private final URL endpoint;
+    private final String json;
+    private final ContextAware appender;
+    private final Charset charset = Charset.forName("UTF-8");
+
+    public RequestRunnable(URL endpoint, String json, ContextAware appender) {
+        this.endpoint = endpoint;
+        this.json = json;
+        this.appender = appender;
+    }
+
+    @Override
+    public void run() {
         try {
             HttpURLConnection connection = (HttpURLConnection) endpoint.openConnection();
 
@@ -26,7 +63,7 @@ public class MessageSender {
             connection.setDoOutput(true);
 
             OutputStream output = connection.getOutputStream();
-            output.write(json.getBytes(Charset.forName("UTF-8")));
+            output.write(json.getBytes(charset));
             output.close();
 
             int statusCode = connection.getResponseCode();
@@ -36,7 +73,6 @@ public class MessageSender {
             connection.disconnect();
         } catch (IOException e) {
             appender.addError("Could not send message", e);
-            e.printStackTrace();
         }
     }
 }
